a60dfe96a6590a402d581f89291d8ef7b1c737ed
themaleem <zizo9585@gmail.com>
2020-07-12 23:53:27 +0100

    added navigation for mobile and desktop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxiliary";
 import styles from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-function Layout(props) {
-  return (
-    <Aux>
-      <div>layout, sidebar, side drawer</div>
-      <main className={styles.Content}>{props.children}</main>
-    </Aux>
-  );
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  // sideDrawerToggleHandler = () => this.setState({showSideDrawer: !this.state.showSideDrawer});
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  
+  render() {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={styles.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
 
-Layout.propTypes = {};
 export default Layout;
