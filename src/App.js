0ef3fde903454f90ddc520a374773018b2163d49
themaleem <zizo9585@gmail.com>
2020-07-23 08:47:28 +0100

    auto login if userToken is still valid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
+
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch } from "react-router-dom";
 import Orders from "./containers/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 
-function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/logout" exact component={Logout} />
-        <Route path="/" exact component={BurgerBuilder} />
-      </Switch>
-    </Layout>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" exact component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+      </Layout>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+export default connect(null, mapDispatchToProps)(App);
